e83499cf23bdec9c468511658dc0acebe468e64e
Mohsen Sadeghbeigi <mohsen.sadegh62@gmail.com>
2017-02-06 11:53:55 +0330

    Add admin user and make some accessibility to pages

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,5 +1,12 @@
 class TutorialsController < ApplicationController
 
+  before_action :find_params, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit]
+
+  def index
+    @tutorials = Tutorial.all
+  end
+
   def new
     @tutorial = Tutorial.new
   end
@@ -14,13 +21,36 @@ class TutorialsController < ApplicationController
   end
 
   def show
-    @tutorial = Tutorial.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @tutorial.update(tutorial_params)
+      flash[:success] = "Tutorial was succssfuly updated"
+      redirect_to @tutorial
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    if @tutorial.destroy
+      redirect_to tutorials_path
+    else
+      render 'show'
+    end
   end
 
   private
 
     def tutorial_params
       params.require(:tutorial).permit(:title, :content)
+    end
+
+    def find_params
+      @tutorial = Tutorial.find(params[:id])
     end
 
 end
--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -1,4 +1,3 @@
 class Tutorial < ApplicationRecord
-  validates :title, presence: true, length: { minimum: 3, maximum: 50 }
-  validates :content, presence: true, length: { minimum: 10, maximum: 300 }
+
 end
