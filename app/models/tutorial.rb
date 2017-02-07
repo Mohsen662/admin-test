bd610b9faec794ae6579b4b5672c472749b044fd
Mohsen Sadeghbeigi <mohsen.sadegh62@gmail.com>
2017-02-07 10:27:41 +0330

    Add images to show pages

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -2,6 +2,6 @@ class Tutorial < ApplicationRecord
   belongs_to :user
   belongs_to :category
 
-  has_attached_file :tutorial_img, styles: { tutorial_index: "300x300>", tutorial_show: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :tutorial_img, styles: { tutorial_index: "250x350>", tutorial_show: "325x475>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :tutorial_img, content_type: /\Aimage\/.*\z/
 end
