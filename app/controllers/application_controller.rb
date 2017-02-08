036346e377c7c9de3da6e7d51eb48daefbfbb078
Mohsen Sadeghbeigi <mohsen.sadegh62@gmail.com>
2017-02-08 17:06:42 +0330

    some changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_filter :store_current_location, :unless => :devise_controller?
   helper_method :logged_in?
 
   def logged_in?
@@ -23,4 +24,11 @@ class ApplicationController < ActionController::Base
       redirect_to root_path
     end
   end
+
+  private
+
+  def store_current_location
+    store_location_for(:user, request.url)
+  end
+  
 end
